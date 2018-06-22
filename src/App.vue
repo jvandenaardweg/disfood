45ab36689652aaaddbaae4298da27fd6e702f219
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-06-22 16:26:56 +0200

    Better layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+html {
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // text-align: center;
+  color: #161616;
+  max-width: 500px;
+  margin: 0 auto;
+  font-size: 16px;
+  line-height: 1.5;
+}
+
+h1 {
+  font-family: 'Merriweather', serif;
+  font-size: 24px;
 }
 #nav {
   padding: 30px;
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -7,12 +7,37 @@
     <hr />
     <button type="button" @click.prevent="handleRemoveIngredient(ingredient)" v-for="ingredient in excludedIngredients" :key="ingredient" class="btn">{{ ingredient }}</button>
     <hr />
-    <p v-if="isLoading">Loading...</p>
-    <div class="recipe" v-for="recipe in recipes" :key="recipe.id">
-      <img :src="recipe.imageLarge" />
-      <h1>{{ recipe.title }}</h1>
+    <!-- <p v-if="isLoading">Loading...</p> -->
+
+    <div class="recipe" v-for="(recipe, index) in recipes" :key="recipe.id" :class="{'is-hidden': index > 0}">
+      <div class="recipe__image">
+        <img :src="recipe.imageLarge" :alt="recipe.title" />
+      </div>
+      <div class="recipe__body">
+        <h1>{{ recipe.title }}</h1>
+
+        <div class="recipe-summary">
+          <div>
+            <span>KCAL</span>
+            <strong>{{ recipe.kcal }}</strong>
+          </div>
+          <div>
+            <span>TIJD</span>
+            <strong>{{ recipe.recipeTime }} min.</strong>
+          </div>
+          <div>
+            <span>PERSONEN</span>
+            <strong>{{ recipe.servingsNumber }} {{ recipe.servingType }}</strong>
+          </div>
+        </div>
+      </div>
+
+      <div class="recipe-footer">
+        <button type="button" class="btn btn-primary btn-block">Bekijk recept</button>
+      </div>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -87,6 +112,8 @@ input[type="text"] {
 
 .recipe {
   position: relative;
+
+
   img {
     width: 100%;
     object-fit: cover;
@@ -95,10 +122,101 @@ input[type="text"] {
   }
 }
 
+.recipe__image {
+  background-color: #E3E3E3;
+  width: 100%;
+  padding-top: 73.05%;
+  position: relative;
+
+  &:before {
+    content: "Loading...";
+    font-weight: bold;
+    font-size: 18px;
+    height: 30px;
+    width: 100px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    color: #9B9B9B;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+
+.is-hidden {
+  display: none;
+}
+
 .btn {
   font-size: 16px;
   padding: 5px 10px;
   font-weight: bold;
   display: inline-block;
+  text-align: center;
+  cursor: pointer;
+
+  &.btn-primary {
+    background-color: #0077FF;
+    color: #ffffff;
+    font-size: 16px;
+    height: 50px;
+    padding: 0 20px;
+    border: 0;
+    border-radius: 3px;
+  }
+
+  &.btn-block {
+    display: block;
+    width: 100%;
+  }
+}
+
+.recipe__body {
+  padding: 20px;
+
+  h1 {
+    margin: 0;
+  }
+}
+.recipe-summary {
+  margin-top: 20px;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  border-top: 1px #D8D8D8 solid;
+  border-bottom: 1px #D8D8D8 solid;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 200px;
+
+  span, strong {
+    display: block;
+  }
+
+  span {
+    font-size: 14px;
+    color: #9B9B9B
+  }
+
+  strong {
+    font-size: 18px;
+    font-weight: bold;
+    color: #4A4A4A;
+  }
+}
+
+.recipe-footer {
+  margin-top: 20px;
+  position: fixed;
+  bottom: 20px;
+  padding: 0 20px;
+  width: 100%;
 }
 </style>
